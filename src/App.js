9a92812dec56eec11b991d3934c88ac651cd98c6
sneha095 <kuriansneha95@gmail.com>
2022-02-15 18:21:21 +0530

    Changes in styles and Images

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Footer from "./components/Footer";
 import FormInput from "./components/FormInput";
 import List from "./components/List";
 import {DataProvider} from "./components/DataProvider";
+import ToDoPic from './Images/check_boxes.svg'
 
 function App() {
 
   return (
     <DataProvider>
     <div className="App">
+    <link href="/index.css" rel="stylesheet"/>
       <h1>To Do List</h1>
       <FormInput></FormInput>
       <List></List>
+      
       <Footer></Footer>
+      <div>
+      <img className="hero-img" src={ToDoPic}/>
+      </div>
     </div>
     </DataProvider>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,7 +28,7 @@ const newTodosComplete = () => {
   return (
     <>
         {
-            todos.length === 0 ? <h2>Congratulations! Nothings To Do</h2> 
+            todos.length === 0 ? <h2>Congratulations! No Tasks!</h2> 
                 :  <div className="row">
                     <label htmlFor="all">
                         <input type="checkbox" name="all" id="all"
